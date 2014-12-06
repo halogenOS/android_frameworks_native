1c06167f14a63496537a4bb94859879377f50d04
Steve Kondik <steve@cyngn.com>
2014-12-05 18:28:32 -0800

    SurfaceFlinger: Native changes to add blur effect
    Native changes to add blur-behind and blur mask effect
    
    Change-Id: I54faf82d750e8299de6d261f6a893ab26d08df84
    
    SurfaceFlinger: Adding template for LayerBlur files
    
    Change-Id: I444009113b7bdd6c5284863fd1f56358e67d9fe6
    
    SurfaceFlinger: Featurize libuiblur module for OSS build
    
    Change-Id: Ifdc176e699434125d17b111c044b8ba954cf717c
    
    sf : Apply user defined panel orientation to blur layer
    
    Blur layer need to capture screen shot to create to blur
    effect. Hence need to take care any user defined orientation
    as well.
    
    Change-Id: I1c0478810d12661edf2586293bbed342a5b4dc7e
    
    surfaceflinger: Cleanups for blur
    
     * Dynamically load it so no direct linkage or headers are required.
    
    Change-Id: I4eba035f527ae4f0fa2485e24a8586145baf0adc
    
    surfaceflinger: Fix uninitialized variable
    
     * And don't try to reinit the lib after a failure.
    
    Change-Id: Ic055a48670ed8cf01e27b16c5d4ddf214db53d20
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- services/surfaceflinger/LayerBlur.cpp
+++ services/surfaceflinger/LayerBlur.cpp
@@ -228,6 +228,7 @@ bool LayerBlur::captureScreen(const sp<const DisplayDevice>& hw, FBO& fbo, Textu
             texture.getTextureName(), 0);
 
     mFlinger->getRenderEngine().clearWithColor(0.0f, 0.0f, 0.0f, 1.0f);
+
     mFlinger->renderScreenImplLocked(
                 hw,
                 Rect(0,0,width,height),
