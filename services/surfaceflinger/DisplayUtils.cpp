1da11c6b715d77389b5f50c9b8770a1cd64fd36c
Steve Kondik <steve@cyngn.com>
2015-10-12 13:44:11 -1000

    surfaceflinger: Clean up use of QC extended API
    gui: only support tertiary display if QTI_BSP is defined
    
    Non-caf display HALs crap out with this
    
    surfaceflinger: Correct QTI_BSP ifdef
    
    * Should be #ifdef, not #if
    
    REF: CRACKLING-938
    
    sf: Fix blending mode for legacy BSP
    
     * After 0c5f3af, glitches when blending is observed on
       A family devices. Modify ifdef to not include QCOM_BSP_LEGACY.
    
    surfaceflinger: Fix the fix of the opaque check fix
    
     * Use the right define.
    
    Change-Id: I45deaffc0d5efd8ffa643120b0dea3aab461d70f

--- services/surfaceflinger/DisplayUtils.cpp
+++ services/surfaceflinger/DisplayUtils.cpp
@@ -42,16 +42,12 @@
 #include "RenderEngine/RenderEngine.h"
 #include "DisplayHardware/FramebufferSurface.h"
 #include "DisplayUtils.h"
+#if QTI_BSP
 #include <ExSurfaceFlinger/ExSurfaceFlinger.h>
 #include <ExSurfaceFlinger/ExLayer.h>
 #include <ExSurfaceFlinger/ExHWComposer.h>
 #include <ExSurfaceFlinger/ExVirtualDisplaySurface.h>
 #include <dlfcn.h>
-#if QTI_BSP
-#include <gralloc_priv.h>
-#endif
-
-#if QTI_BSP
 #include <gralloc_priv.h>
 #endif
 
@@ -75,31 +71,34 @@ DisplayUtils* DisplayUtils::getInstance() {
 }
 
 SurfaceFlinger* DisplayUtils::getSFInstance() {
+#ifdef QTI_BSP
     if(sUseExtendedImpls) {
         return new ExSurfaceFlinger();
-    } else {
-        return new SurfaceFlinger();
     }
+#endif
+    return new SurfaceFlinger();
 }
 
 Layer* DisplayUtils::getLayerInstance(SurfaceFlinger* flinger,
                             const sp<Client>& client, const String8& name,
                             uint32_t w, uint32_t h, uint32_t flags) {
+#ifdef QTI_BSP
     if(sUseExtendedImpls) {
         return new ExLayer(flinger, client, name, w, h, flags);
-    } else {
-        return new Layer(flinger, client, name, w, h, flags);
     }
+#endif
+    return new Layer(flinger, client, name, w, h, flags);
 }
 
 HWComposer* DisplayUtils::getHWCInstance(
                         const sp<SurfaceFlinger>& flinger,
                         HWComposer::EventHandler& handler) {
+#ifdef QTI_BSP
     if(sUseExtendedImpls) {
         return new ExHWComposer(flinger, handler);
-    } else {
-        return new HWComposer(flinger,handler);
     }
+#endif
+    return new HWComposer(flinger,handler);
 }
 
 void DisplayUtils::initVDSInstance(HWComposer* hwc, int32_t hwcDisplayId,
@@ -108,6 +107,7 @@ void DisplayUtils::initVDSInstance(HWComposer* hwc, int32_t hwcDisplayId,
         sp<IGraphicBufferConsumer> bqConsumer, String8 currentStateDisplayName,
         bool currentStateIsSecure, int currentStateType)
 {
+#ifdef QTI_BSP
     if(sUseExtendedImpls) {
         if(hwc->isVDSEnabled()) {
             VirtualDisplaySurface* vds = new ExVirtualDisplaySurface(*hwc, hwcDisplayId,
@@ -123,11 +123,16 @@ void DisplayUtils::initVDSInstance(HWComposer* hwc, int32_t hwcDisplayId,
             producer = vds;
         }
     } else {
+#endif
+        (void)currentStateIsSecure;
+        (void)currentStateType;
         VirtualDisplaySurface* vds = new VirtualDisplaySurface(*hwc, hwcDisplayId,
                 currentStateSurface, bqProducer, bqConsumer, currentStateDisplayName);
         dispSurface = vds;
         producer = vds;
+#ifdef QTI_BSP
     }
+#endif
 }
 
 bool DisplayUtils::createV4L2BasedVirtualDisplay(HWComposer* hwc, int32_t &hwcDisplayId,
@@ -175,7 +180,7 @@ bool DisplayUtils::canAllocateHwcDisplayIdForVDS(int usage) {
     property_get("debug.vds.allow_hwc", value, "0");
     int allowHwcForVDS = atoi(value);
 
-#if QTI_BSP
+#ifdef QTI_BSP
     // Do not allow hardware acceleration
     flag_mask = GRALLOC_USAGE_PRIVATE_WFD;
 #endif
